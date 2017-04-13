051f0efdc1036ede73df4640bd1a6baa5e12f528
JDjedi <jcdiaz1201@gmail.com>
2017-04-12 22:05:49 -0500

    Node project completed

--- index.js
+++ index.js
@@ -3,15 +3,17 @@
 var chalk = require('chalk');
 var _ = require('lodash');
 var logger = require('./lib/logger');
+var data = require('./data/logs')
 
-var array = ['This message is info', 'This mesage is a warning', 'This message is an error'];
+// var array = ['This message is info', 'This mesage is a warning', 'This message is an error'];
+// logger.log(array[0], 'info');
+// logger.log(array[1], 'warning');
+// logger.log(array[2], 'error');
 
-// _.each(array, function(array){
-// 	logger.log(array);
-// });
+_.each(data, function(data){
+	logger.log(data.level, data.message);
+});
 
-logger.log(array[0], 'INFO');
-logger.log(array[1], 'WARNING');
-logger.log(array[2], 'ERROR');
+
 
 
