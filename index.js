e9dbb0f76cb3ddc531f6cb0798e9d8d36fd3add1
JDjedi <jcdiaz1201@gmail.com>
2017-04-12 21:27:23 -0500

    Part 2 of node project completed

--- index.js
+++ index.js
@@ -2,11 +2,16 @@
 
 var chalk = require('chalk');
 var _ = require('lodash');
+var logger = require('./lib/logger');
 
-var array = ['elephant', 'dog', 'cat', 'bear', 'penguin', 'ant', 'seal'];
+var array = ['This message is info', 'This mesage is a warning', 'This message is an error'];
 
-_.each(array, function(array){
-	console.log(chalk.green(array));
-});
+// _.each(array, function(array){
+// 	logger.log(array);
+// });
+
+logger.log(array[0], 'INFO');
+logger.log(array[1], 'WARNING');
+logger.log(array[2], 'ERROR');
 
 
